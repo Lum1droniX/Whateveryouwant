630541f6beecaa5f7c201ca5a5221e5874b99496
Lum1droniX <haydfill710@gmail.com>
2026-09-09 14:01:20 -0600

    Starting Project1

---
+++ Project1/debug-1.py
@@ -0,0 +1,13 @@
+# Intro to Programming
+# Debug Exercise 2
+
+# This program should add three numbers together.
+
+total = 0
+num1 = input("What's the first number? >")
+total1 = total + int(num1)
+num2 = input("What's the second number? >")
+total2 = total1 + int(num2)
+num3 = input("What's the third number? >")
+total3 = total2 + int(num3)
+print(f"Total is: {total3}")
