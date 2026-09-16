e0546885eb43909229c75ed35f637c19dabdc7a0
Lum1droniX <haydfill710@gmail.com>
2026-09-16 14:26:19 -0600

    Week 5 Lab and Notes

---
+++ Week5Lab/BranchingLab.py
@@ -0,0 +1,37 @@
+# Grading system
+
+PercentGrade = float(input("What grade percentage did you get? "))
+if PercentGrade < 60:
+    print("You recieved an F!")
+elif PercentGrade >= 60 and PercentGrade < 70:
+    print("You recieved an D!")
+elif PercentGrade >= 70 and PercentGrade < 80:
+    print("You recieved an C!")
+elif PercentGrade >= 70 and PercentGrade < 90:
+    print("You recieved an B!")
+else:
+    print("You recieved an A!")
+
+# Temperature converter
+
+TempType = input("What type of temperature are you converting (Celcius or Farenheit)? ").upper()
+TempValue = float(input("What is the temperature value? "))
+
+CelcToFar = (TempValue * (9/5)) + 35
+FarToCelc = (TempValue - 35) * (5/9)
+
+if TempType == "CELCIUS":
+    print("Converting from Celcius to Farenheit")
+    print(f"Your value in Farenheit is {CelcToFar}")
+else:
+    print("Converting from Farenheit to Celcius")
+    print(f"Your value in Celcius is {FarToCelc}")
+
+# Password checker
+
+Password = input("What is your password? ")
+if len(Password) > 7:
+    print("Password is too small!")
+    print("Password must be at least 8 characters")
+else:
+    print("Password accepted!")
---
+++ Week5Lab/week5notes.py
@@ -0,0 +1,71 @@
+# What is a boolean?
+# A boolean value is either True or False
+# Examples:
+    # print(5<10)
+    # print(5>10)
+    # print(3>=7)
+    # print('cat'=='dog')
+
+# answer=input("Do you want to continue? (yes or no) ").lower()
+# if answer=="yes":
+#     print("Continuing the program...")
+# elif answer=="no":
+#     print("How come?")
+# else:
+#     print("Exiting program...")
+
+# Comparison operators
+    # == equal to
+    # != not equal to
+    # > greater than
+    # < less than
+    # <= less than or equal to
+    # >= greather than or equal to
+
+# score=85
+# print(score>75)
+# print(score==75)
+# print(score!=85)
+
+# How do we compare 3 values?
+# We can compare 3 or more values with "and" and "or"
+
+# temp=72
+# if temp >= 68 and temp <= 75:
+#     print("Comfy room temp")
+
+# When using AND, both conditions must be true
+# When using OR, at least one condition must be true
+
+# day="Saturday"
+# is_holiday=False
+# if day=="Saturday" or day=="Sunday" or is_holiday:
+#     print("You don't have class today")
+
+# I want to print "You don't have class today"
+
+# score=75
+# if score <= score <90:          # score >= 60 and score < 90
+#     print("Mediocre grade")
+
+# Give me a conditional program that prints out shipping cost depending on total price
+
+# TotalPrice = float(input("What is your total price?"))
+# if TotalPrice < 25: 
+#     print("Shipping is $10")
+# elif TotalPrice > 50:
+#     print("Shipping is $5")
+# else:
+#     print("Shipping is free")
+
+# list1=[1,2,3]
+# list2=[1,2,3]
+# list3=list1
+
+# print(list1==list2)
+# print(list1 is list2)
+# print(list1==list3)
+# print(list1 is list3)
+
+# == compares values and contents
+# "is" compares identities
