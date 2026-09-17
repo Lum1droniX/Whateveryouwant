5c1a99485d2ad3c2483c039c7a70b800cc8d9103
Lum1droniX <haydfill710@gmail.com>
2026-09-16 22:49:05 -0600

    Project1 debug

--- Project1/debug-1.py
+++ Project1/debug-1.py
@@ -3,11 +3,13 @@
 
 # This program should add three numbers together.
 
-total = 0
-num1 = input("What's the first number? >")
-total1 = total + int(num1)
-num2 = input("What's the second number? >")
-total2 = total1 + int(num2)
-num3 = input("What's the third number? >")
-total3 = total2 + int(num3)
-print(f"Total is: {total3}")
+# To fix this program, I first got rid of the total variable at the start and set it to an equation at the bottom.
+# I then added float() to each input so that their values could support decimals and be used in the total variable's equation.
+# As for the new "total" equation, I made it equal the sum of each float input.
+# The result of these changes is a program that adds the three input numbers together.
+
+num1 = float(input("What's the first number? >"))
+num2 = float(input("What's the second number? >"))
+num3 = float(input("What's the third number? >"))
+total = num1 + num2 + num3
+print(f"Total is: {total}")
